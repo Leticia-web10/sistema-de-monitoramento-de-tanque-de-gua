bef46b2392fce73274bd5a513631c12ffda559c7
Leticia Wang <leticia.wang10@outlook.com>
2026-09-08 13:23:13 -0300

    Update issue 01 .py

--- issue 01 .py
+++ issue 01 .py
@@ -1,19 +1,26 @@
+import sqlite3
+
 class Tanque:
     def __init__(self, nome, capacidade):
         self.nome = nome
         self.capacidade = capacidade
-        self.nivel_atual = 0
 
-    def exibir(self):
-        print(f"Tanque: {self.nome}")
-        print(f"Capacidade: {self.capacidade} L")
-        print(f"Nível atual: {self.nivel_atual} L")
+    def salvar(self):
+        conexao = sqlite3.connect("tanques.db")
+        cursor = conexao.cursor()
 
+        cursor.execute("""
+            INSERT INTO tanques (nome, capacidade)
+            VALUES (?, ?)
+        """, (self.nome, self.capacidade))
+
+        conexao.commit()
+        conexao.close()
+
+        print("Tanque cadastrado com sucesso!")
+#teste
 nome = input("Nome do tanque: ")
-capacidade_input = input("Capacidade (500L): ")
-capacidade = float(capacidade_input) if capacidade_input.strip() else 500.0
+capacidade = float(input("Capacidade (L): "))
 
 tanque = Tanque(nome, capacidade)
-
-print("\nTanque foi cadastrado")
-tanque.exibir()
+tanque.salvar()
